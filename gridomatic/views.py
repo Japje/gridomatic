b30a25735fa851d489cb8619e3e45884e6667f56
Sebastian Wiedenroth <wiedi@frubar.net>
2014-01-16 11:07:00 +0100

    move some xen stuff into seperate file

--- gridomatic/views.py
+++ gridomatic/views.py
@@ -2,99 +2,24 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.conf import settings
 import XenAPI
 from forms import VMCreateForm
-
-def create_session():
-        try:
-                session = XenAPI.Session(settings.XEN_URL)
-                session.xenapi.login_with_password(settings.XEN_USER, settings.XEN_PASSWORD)
-        except Exception, e:
-                if e.details[0] == 'HOST_IS_SLAVE':
-                        # Redirect to cluster master
-                        url = urlparse(url).scheme + '://' + e.details[1]
-                        try:
-                                session = XenAPI.Session(url)
-                                session.login_with_password(settings.XEN_USER, settings.XEN_PASSWORD)
-                        except Exception, e:
-                                handle_login_error(e)
-                else:
-                        handle_login_error(e)
-        return session
-
-
-
-def get_network_list(session):
-	networks = session.xenapi.network.get_all_records()
-	network_list = []
-	for net in networks:
-		if not 'Production' in networks[net]['tags']: continue
-		network_list += [(networks[net]['uuid'], networks[net]['name_label'], networks[net]['name_description'])]
-	return network_list
-
-def get_template_list(session):
-	templates = session.xenapi.VM.get_all_records()
-	template_list = []
-	for tpl in templates:
-		if not 'Production' in templates[tpl]['tags']: continue
-		if not templates[tpl]["is_a_template"]: continue
-		template_list += [(templates[tpl]['uuid'], templates[tpl]['name_label'])]
-	return template_list
-
-def get_host_list(session):
-	hosts = session.xenapi.host.get_all_records()
-	host_list = []
-	for host in hosts:
-		host_list += [(hosts[host]['name_label'], hosts[host]['name_label'])]
-	return host_list
+from .xen import *
 
 def vm_list(request):
-	session = create_session()
-	vms = session.xenapi.VM.get_all_records()
-	vm_list = []
-	for ref, vm in vms.items():
-		if vm["is_a_template"] or vm['is_a_snapshot'] or vm["is_control_domain"]: continue
-
-		vm_list += [{
-			'name': vm['name_label'],
-			'power_state': vm['power_state'],
-			'uuid': vm['uuid']
-		}]
-	return render(request, 'gridomatic/vm_list.html', {'vms': vm_list})
+	return render(request, 'gridomatic/vm_list.html', {'vms': Xen().vm_list()})
 
 def vm_start(request):
 	uuid = request.POST.get('uuid', None)
-	if not uuid:
-		return redirect('vm_list')
-	session = create_session()
-	ref = session.xenapi.VM.get_by_uuid(uuid)
-	try:
-		session.xenapi.VM.start(ref, False, True)
-	except:
-		pass
+	Xen().vm_start(uuid)
 	return redirect('vm_list')
 
 def vm_stop(request):
 	uuid = request.POST.get('uuid', None)
-	if not uuid:
-		return redirect('vm_list')
-	session = create_session()
-	ref = session.xenapi.VM.get_by_uuid(uuid)
-	try:
-		session.xenapi.VM.clean_shutdown(ref)
-	except:
-		pass
+	Xen().vm_stop(uuid)
 	return redirect('vm_list')
 
 def vm_restart(request):
 	uuid = request.POST.get('uuid', None)
-	if not uuid:
-		return redirect('vm_list')
-	session = create_session()
-	ref = session.xenapi.VM.get_by_uuid(uuid)
-	try:
-		session.xenapi.VM.clean_shutdown(ref)
-		session.xenapi.VM.start(ref, False, True)
-	except:
-		pass
+	Xen().vm_restart(uuid)
 	return redirect('vm_list')
 
 def deploy(name, ip, gw, netmask, ns, network, template, host, ip6, gw6, netmask6, sshkey):
---
+++ gridomatic/xen.py
@@ -0,0 +1,79 @@
+from django.conf import settings
+import XenAPI
+
+class Xen():
+	def __init__(self):
+		try:
+			session = XenAPI.Session(settings.XEN_URL)
+			session.xenapi.login_with_password(settings.XEN_USER, settings.XEN_PASSWORD)
+		except Exception, e:
+			if e.details[0] == 'HOST_IS_SLAVE':
+				# Redirect to cluster master
+				url = urlparse(url).scheme + '://' + e.details[1]
+				session = XenAPI.Session(url)
+				session.login_with_password(settings.XEN_USER, settings.XEN_PASSWORD)
+			else:
+				raise Exception(e)
+		self.session = session
+
+	def get_network_list(self):
+		networks = self.session.xenapi.network.get_all_records()
+		network_list = []
+		for net in networks:
+			if not 'Production' in networks[net]['tags']: continue
+			network_list += [(networks[net]['uuid'], networks[net]['name_label'])]
+		return network_list
+
+	def get_template_list(self):
+		templates = self.session.xenapi.VM.get_all_records()
+		template_list = []
+		for tpl in templates:
+			if not 'Production' in templates[tpl]['tags']: continue
+			if not templates[tpl]["is_a_template"]: continue
+			template_list += [(templates[tpl]['uuid'], templates[tpl]['name_label'])]
+		return template_list
+
+	def get_host_list(self):
+		hosts = self.session.xenapi.host.get_all_records()
+		host_list = []
+		for host in hosts:
+			host_list += [(hosts[host]['name_label'], hosts[host]['name_label'])]
+		return host_list
+	
+	def vm_list(self):
+		vms = self.session.xenapi.VM.get_all_records()
+		vm_list = []
+		for ref, vm in vms.items():
+			if vm["is_a_template"] or vm['is_a_snapshot'] or vm["is_control_domain"]: continue
+
+			vm_list += [{
+				'name':        vm['name_label'],
+				'power_state': vm['power_state'],
+				'uuid':        vm['uuid'],
+			}]
+		return vm_list
+	
+	def vm_start(self, uuid):
+		ref = self.session.xenapi.VM.get_by_uuid(uuid)
+		try:
+			self.session.xenapi.VM.start(ref, False, True)
+		except:
+			pass
+	
+	def vm_stop(self, uuid):
+		ref = self.session.xenapi.VM.get_by_uuid(uuid)
+		try:
+			self.session.xenapi.VM.clean_shutdown(ref, False, True)
+		except:
+			pass
+	
+	def vm_restart(self, uuid):
+		ref = self.session.xenapi.VM.get_by_uuid(uuid)
+		try:
+			self.session.xenapi.VM.clean_shutdown(ref)
+			self.session.xenapi.VM.start(ref, False, True)
+		except:
+			pass
+	
+	def vm_deploy(self, options):
+		pass
