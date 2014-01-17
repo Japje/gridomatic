faa08594e4642e8d2cba816732fb62cc46f30bea
Sebastian Wiedenroth <wiedi@frubar.net>
2014-01-17 11:14:06 +0100

    typo in form

--- gridomatic/forms.py
+++ gridomatic/forms.py
@@ -6,7 +6,7 @@ class VMCreateForm(forms.Form):
 	ip_address  = forms.GenericIPAddressField(protocol='ipv4', label="IP Address") 
 	netmask     = forms.GenericIPAddressField(protocol='ipv4')
 	gateway     = forms.GenericIPAddressField(protocol='ipv4')
-	dns         = forms.CharField(help_text="List of space seperated Nameserver Adresses", label="DNS Servers")
+	dns         = forms.CharField(help_text="List of space seperated Nameserver Addresses", label="DNS Servers")
 
 	ip_address6 = forms.GenericIPAddressField(protocol='ipv6', required=False, label="IPv6 Address")
 	netmask6    = forms.IntegerField(label='IPv6 Prefixlength', required=False, min_value=1, initial=64, max_value=128)
