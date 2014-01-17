789c0b5d7bbd2cfcb20f5afa6ef6f47d365c10e7
Sebastian Wiedenroth <wiedi@frubar.net>
2014-01-17 11:11:43 +0100

    more form details

--- gridomatic/forms.py
+++ gridomatic/forms.py
@@ -17,23 +17,24 @@ class VMCreateForm(forms.Form):
 	network     = forms.ChoiceField(choices = [])
 	host        = forms.ChoiceField(choices = [])
 
-	mem_size    = forms.IntegerField(help_text="Size in MB", min_value=128)
-	cpu_cores   = forms.IntegerField(initial=1, min_value=1)
+	mem_size    = forms.IntegerField(label="Memory Size", help_text="Size in MB", min_value=128)
+	cpu_cores   = forms.IntegerField(label="CPU Cores", initial=1, min_value=1)
 
 	sshkey      = forms.CharField(help_text="In OpenSSH format", label="SSH Public Key")
 
-class NetworkCreateForm(forms.Form):
-	name        = forms.CharField(help_text="Name for the new Network", label="Name")
-	description = forms.CharField(help_text="Please provide a detailed description!", label="Description")
-	vlan        = forms.IntegerField()
 
+class NetworkCreateForm(forms.Form):
+	name        = forms.CharField(help_text="Name for the new Network")
+	description = forms.CharField(help_text="Please provide a detailed description")
+	vlan        = forms.IntegerField(label="VLAN")
 
 class VMEditForm(forms.Form):
-	description	= forms.CharField(help_text="Please provide a detailed description!", label="Description")
-	mem_size        = forms.IntegerField(help_text="Size in MB", min_value=128)
-	cpu_cores       = forms.IntegerField(initial=1, min_value=1)
+	description = forms.CharField(help_text="Please provide a detailed description", label="Description")
+	mem_size    = forms.IntegerField(label="Memory Size", help_text="Size in MB", min_value=128)
+	cpu_cores   = forms.IntegerField(label="CPU Cores", initial=1, min_value=1)
 
 class NetworkEditForm(forms.Form):
-	name		= forms.CharField(help_text="Please provide a name!", label="name")
-	description	= forms.CharField(help_text="Please provide a detailed description!", label="Description")
-	racktables_id	= forms.CharField(help_text="network_id from racktables", label="Racktables_id")
+	name          = forms.CharField(help_text="Please provide a name")
+	description   = forms.CharField(help_text="Please provide a detailed description")
+	racktables_id = forms.CharField(help_text="network_id from racktables", label="Racktables ID")
+
